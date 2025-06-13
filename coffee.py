6930377a7857d7e0d17dfb6a7ec60dcb67925ba6
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-06-13 00:42:26 -0500

    Prompt user for input

--- coffee.py
+++ coffee.py
@@ -1,6 +1,10 @@
-class coffee:
+class CoffeeMachine:
     def __init__(self):
         self._water = 300 # ml
         self._milk = 200 # ml
         self._coffee = 100 # ml
         self._money = 0 # dollars
+    
+    def prompt_user(self) -> str:
+        user_choice = input("What would you like (espresso/latte/cappuccino)?\n> ").lower()
+        return user_choice
